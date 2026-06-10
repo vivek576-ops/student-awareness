fac88c05e022119f8d8d886de28e7cff7d9a8f06
ManiVivek <manivivek.12346@gmail.com>
2026-06-10 08:21:44 +0530

    Remove temporary hash files

--- backend/generate_hash.py
+++
@@ -1,5 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-hash = generate_password_hash('principal123', method='pbkdf2:sha256')
-print("HASH:", hash)
-print("CHECK:", check_password_hash(hash, 'principal123'))
